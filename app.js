b3ef4a61004681ef7be5537201dd4c5901d9ab3d
Vega <valentin.ph.michel@gmail.com>
2023-12-17 23:08:44 +0100

    add feature

--- app.js
+++ app.js
@@ -10,15 +10,68 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Parse JSON bodies (as sent by API clients)
 app.use(bodyParser.json());
 
+// Configure express to use public folder
+app.use(express.static('public'));
+
 // Configure express to use EJS
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.render('index', { message: 'Hello, world!' });
+    res.send('Hello world');
+});
+
+app.get('/root', (req, res) => {
+    res.send('Login successful!');
 });
 
 app.get('/login', (req, res) => {
-    res.send('Login successful!');
+    console.log('This is a message for the logs.');
+    res.render('index');
+});
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    console.log(username);
+    const password = req.body.password;
+    console.log(password);
+    // Check if the login is valid
+    if (username === 'admin' && password === 'password') {
+        // If it is, redirect to the root page
+        res.redirect('/root');
+    } else {
+        // If it isn't, redirect back to the login page
+        res.redirect('/login');
+    }
+});
+
+const fs = require('fs');
+const path = require('path');
+
+// Route pour afficher les répertoires
+app.get('/directories', (req, res) => {
+    fs.readdir('./data', (err, directories) => {
+        if (err) {
+            console.error("Could not list the directory.", err);
+            res.status(500).send('An error occurred');
+        } else {
+            res.render('directories', { directories });
+        }
+    });
+});
+
+// Route pour afficher les fichiers d'un répertoire spécifique
+app.get('/directories/:dir', (req, res) => {
+    const dirPath = path.join('./data', req.params.dir);
+    fs.readdir(dirPath, (err, files) => {
+        if (err) {
+            console.error("Could not list the directory.", err);
+            res.status(500).send('An error occurred');
+        } else {
+            res.render('files', { files });
+        }
+    });
 });
 
 app.listen(port, () => {
--- launch_node_server.sh
+++ launch_node_server.sh
@@ -1,4 +1,7 @@
-#/bin/bash
+#!/bin/bash
+
+# launch nginx server
+sudo service nginx start
 
 # install express for node server
 npm install express
@@ -6,4 +9,7 @@ npm install express
 # install ejs for node views
 npm install ejs
 
+# launch node server
 node app.js
+
+sudo service nginx stop
